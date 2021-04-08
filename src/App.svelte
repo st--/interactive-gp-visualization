10cf01043dfcad60c5e448e5734562df1dba3f42
ST John <st--@users.noreply.github.com>
2021-04-08 13:44:32 +0100

    update todos, add links to other visualisations

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,36 @@
 <!--
 To dos:
 - proper spacing/(re)sizing of Covariance/Line/Kernel plots
-- axis & line labels
+- axis & line labels (LaTeX/ketex?)
 - Covariance plot: square aspect ratio
 - Covariance plot: fix covariance ellipse
 - Kernel plot: automatic y-axis scaling?
 - change Line plot to allow changing $x1 (with Shift+Click?)
+- marginal distribution plots for Covariance and Lineplot
+- Lineplot: 1 and 2 sigma
 
 More features:
+- checkbox for showing/hiding mean/credible intervals
 - select prior mean function (linear, quadratic, sine?)
 - select prior kernel function (sqexp, mat32, mat12, periodic?)
 - adjustable likelihood noise scale (input slider?)
+- smoothly animated samples (see http://mlss.tuebingen.mpg.de/2013/Hennig_2013_Animating_Samples_from_Gaussian_Distributions.pdf)
+- include log marginal likelihood
+- include 2D visualisation of covariance function
+
+
+Alternative visualisations:
+https://distill.pub/2019/visual-exploration-gaussian-processes/ https://github.com/distillpub/post--visual-exploration-gaussian-processes
+http://smlbook.org/GP/ https://github.com/uu-sml/sml-book-page/tree/master/GP
+http://www.tmpl.fi/gp/ https://github.com/to-mi/gp-demo-js
+http://chifeng.scripts.mit.edu/stuff/gp-demo/ https://github.com/chi-feng/gp-demo
+https://gaussianprocess.herokuapp.com/ https://github.com/aybchan/gaussianprocess
+https://github.com/awav/interactive-gp (Python-based)
 
 Future thoughts:
 - per-data point noise scale - how to do UI?
-- entered by click-and-pull when creating data points on main plot
+  entered by click-and-pull when creating data points on main plot
+- optimize hyperparameters
 -->
 <script lang="ts">
   import Lineplot from "./Lineplot.svelte";
@@ -23,12 +39,7 @@ Future thoughts:
   import RandomSample from "./RandomSample.svelte";
   import { x1, x2, vs } from "./store.js";
   import { sqexp, matern12, white, sumKernel } from "./kernels.js";
-  import {
-    linspace,
-    matrixSqrt,
-    sampleMvn,
-    covEllipse,
-  } from "./mymath.js";
+  import { linspace, matrixSqrt, sampleMvn, covEllipse } from "./mymath.js";
   import { getIndexInSorted } from "./binarysearch.js";
   import { posterior, prior } from "./gpposterior.js";
 
@@ -73,11 +84,14 @@ Future thoughts:
 
   <div>
     <h3>Instructions</h3>
-    Top-left: visualises slice through covariance function k(x1, .) where x1 can be changed by clicking (red line).
-    Right: visualises covariance k(x1, x2) where x1 can be changed by clicking in top-left plot and x2 follows mouse in top-left and bottom-left plots (orange line).
-    Dots correspond to function values at the samples in the bottom-left plot.
-    Bottom-left: visualises mean (solid blue line), marginal variance (shaded blue area), and samples from the GP. Clicking adds data points (black circles); clicking on an existing point removes it.
-    Controls: change number of samples; re-draw random samples; remove all points.
+    Top-left: visualises slice through covariance function k(x1, .) where x1 can
+    be changed by clicking (red line). Right: visualises covariance k(x1, x2) where
+    x1 can be changed by clicking in top-left plot and x2 follows mouse in top-left
+    and bottom-left plots (orange line). Dots correspond to function values at the
+    samples in the bottom-left plot. Bottom-left: visualises mean (solid blue line),
+    marginal variance (shaded blue area), and samples from the GP. Clicking adds
+    data points (black circles); clicking on an existing point removes it. Controls:
+    change number of samples; re-draw random samples; remove all points.
   </div>
 
   <div style="display: grid;">
